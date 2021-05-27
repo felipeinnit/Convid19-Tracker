aadc509a4c2f43d5b064fd2d536b9dfbfa7dff7e
Felipe Ribeiro <eusouocara3000@gmail.com>
2021-05-27 14:47:45 +0100

    Update LineGraph.js

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import {Card, CardContent, Typography} from '@material-ui/core';
-import './InfoBox.css';
+import React, {useState,useEffect} from 'react';
+import {Line} from 'react-chartjs-2';
+import './LineGraph.css';
 
-function InfoBox({title, cases, total}) {
-    return (
-        <Card className={"infoBox"}
-          >
-            <CardContent>
-                <Typography className="infoBox_tittle ">
-                {title}
-                </Typography>
+const buildChartData = (data, casesType) => {
+    let chartData = [];
+    let lastDataPoint;
+    for (let date in data.cases) {
+      if (lastDataPoint) {
+        let newDataPoint = {
+          x: date,
+          y: data[casesType][date] - lastDataPoint,
+        };
+        chartData.push(newDataPoint);
+      }
+      lastDataPoint = data[casesType][date];
+    }
+    return chartData;
+  };
 
-                <h2 className="infoBox_cases">
-                {cases}
-                </h2>
 
-                <Typography lassName="infoBox_total" >
-                {total} Total
-                </Typography>
-            </CardContent>
-        </Card>
+  function LineGraph({ casesType = "cases" }) {
+    const [data, setData] = useState({});
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            let chartData = buildChartData(data, casesType);
+            setData(chartData);
+          });
+      };
+  
+      fetchData();
+    }, [casesType]);
+
+return (
+        <div>
+             {data?.length > 0 && (
+            <Line className={"lineGraph"}
+            data={{
+                datasets: [{
+                    backgroundColor: "#FD6362",
+                    color: "red",
+                    borderColor: "#FD6362",
+                    data:data,
+                    label:"daily cases",
+                  
+                },],
+            }} 
+            /> 
+            )}
+        </div>
     )
 }
 
-export default InfoBox
+export default LineGraph
