c0a95e3c6d6caa7696fa7808383d5579455efc04
Felipe Ribeiro <eusouocara3000@gmail.com>
2021-05-27 14:46:39 +0100

    Update LineGraph.js

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,90 +1,26 @@
-import React, {useState,useEffect} from 'react';
-import {Line} from 'react-chartjs-2';
+import React from 'react';
+import {Card, CardContent, Typography} from '@material-ui/core';
+import './InfoBox.css';
 
-// function LineGraph() {
-//     const [data, setData] = useState({});
+function InfoBox({title, cases, total}) {
+    return (
+        <Card className={"infoBox"}
+          >
+            <CardContent>
+                <Typography className="infoBox_tittle ">
+                {title}
+                </Typography>
 
-//     const buildChartData = (data, casesType = "cases") => {
-//         const chartData = [];
-//         let lastDataPoint;
-//         for (let date in data.cases) {
-//           if (lastDataPoint) {
-//             let newDataPoint = {
-//               x: date,
-//               y: data[casesType][date] - lastDataPoint,
-//             };
-//             chartData.push(newDataPoint);
-//           };
-//           lastDataPoint = data[casesType][date];
-//         };
-//         return chartData;
-//       };
+                <h2 className="infoBox_cases">
+                {cases}
+                </h2>
 
-// useEffect(() => {
-//     const fetchData = async () => {
-
-//     await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-//     .then((response) => response.json())
-//     .then((data) =>{
-//         let chartData = buildChartData(data, "cases");
-//         setData(chartData);
-//     });
-//     };
-//     fetchData();
-// },[]);
-
-const buildChartData = (data, casesType) => {
-    let chartData = [];
-    let lastDataPoint;
-    for (let date in data.cases) {
-      if (lastDataPoint) {
-        let newDataPoint = {
-          x: date,
-          y: data[casesType][date] - lastDataPoint,
-        };
-        chartData.push(newDataPoint);
-      }
-      lastDataPoint = data[casesType][date];
-    }
-    return chartData;
-  };
-  
-  function LineGraph({ casesType = "cases" }) {
-    const [data, setData] = useState({});
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            let chartData = buildChartData(data, casesType);
-            setData(chartData);
-          });
-      };
-  
-      fetchData();
-    }, [casesType]);
-
-return (
-        <div>
-             {data?.length > 0 && (
-            <Line 
-            data={{
-                datasets: [{
-                    backgroundColor: "rgba(204, 16, 52, 0.5)",
-                    borderColor: "#CC1034",
-                    data:data,
-                    label:"daily cases",
-                },],
-            }} 
-            /> 
-            )}
-        </div>
+                <Typography lassName="infoBox_total" >
+                {total} Total
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
-export default LineGraph
-
-
+export default InfoBox
